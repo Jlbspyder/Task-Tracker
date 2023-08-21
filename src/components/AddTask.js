f77b1c8903aeaaf3a34fb16c8bb1ca02c8c0500e
Debo Gbadebo <111382307+Jlbspyder@users.noreply.github.com>
2023-08-21 14:26:55 +0100

    Update AddTask.js

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -4,16 +4,14 @@ const AddTask = ({ onAdd }) => {
    const [text, setText] = useState('')
    const [day, setDay] = useState('') 
    const [reminder, setReminder] = useState(false)  
-   const [error, setError] = useState({}) 
+   const [textError, setTextError] = useState(false) 
+   const [dayError, setDayError] = useState(false) 
 
    const onSubmit = (e) => {
     e.preventDefault()
     if (!text || !day) {
-        setError(prev => ({
-            ...prev,
-            task: true,
-            time: true
-        }))
+        setTextError(true);
+        setDayError(true)
         }
       return;
     } 
@@ -21,7 +19,8 @@ const AddTask = ({ onAdd }) => {
 
     setText('')
     setDay('')
-    setError({})
+    setTextError(false)
+    setDayError(false)
     setReminder(false)
    }
   return (
@@ -34,7 +33,7 @@ const AddTask = ({ onAdd }) => {
              className={error.task && "form-control error"}
              value={text}
              onChange={(e) => setText(e.target.value)} />
-             {error.task ? <p className='error'>Please add a Task</p> : ""}
+             {textError ? <p className='error'>Please add a Task</p> : ""}
         </div>
         <div className="form-control">
             <label className={error.time && "error"}>Day & Time</label>
@@ -45,7 +44,7 @@ const AddTask = ({ onAdd }) => {
              className={error.time && "form-control error"}
              value={day}
              onChange={(e) => setDay(e.target.value)} />
-             {error.time ? <p className='error'>Please add a Day & Time</p> : ""}
+             {dayError ? <p className='error'>Please add a Day & Time</p> : ""}
         </div>
         <div className="form-control form-control-check">
             <label>Set Reminder</label>
